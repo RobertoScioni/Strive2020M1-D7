ac981e6f6e36939180d8cdd253f4daf7999fbb87
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-10-06 14:04:27 +0200

    imported M1-D6 FILES

---
+++ M1-D6.js
@@ -0,0 +1,20 @@
+send=()=>{
+    message=document.createTextNode(document.querySelector("#message").value)
+    document.querySelector("#message").value=""
+    line=document.createElement("p")
+    line.appendChild(message)
+    document.querySelector("#chat").appendChild(line)
+}
+
+eEgg=()=>{
+    document.querySelector("h1").innerText="One Store to rule them all, One Store to find them, One Store to bring them all, and in the darkness bind them"    
+}
+
+pgBg=(color)=>{
+    document.querySelector("body").style.backgroundColor=color
+}
+
+maskAddress=()=>{
+    document.querySelector("address").innerText="4626  Stonepot Road,Belle Mead"
+    document.querySelector("#MAP").remove()
+}
