d2100bb4256ecb13e448abfd0518889eab3430c8
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-10-06 14:54:30 +0200

    exercises 1 to 4 done

--- M1-D7.js
+++ M1-D7.js
@@ -1,3 +1,29 @@
-onload():{
+//Exercise 2
+    const changeTitle = (newTitle)=> {
+        document.querySelector("title").innerText="A wizard is never late, nor is he early, he arrives precisely when he means to."
+    };
+//Exercise 3 a
+    const addClassToTitle=()=>{
+        document.querySelector("H1").classList.add("red-color")
+    };
+//Exercise 3 b
+    const removeClassFromTitle=()=>{
+        document.querySelector("H1").classList.remove("red-color")
+    }
+ 
+//exercise 4
+    const changeP=()=>{
+        ps=document.querySelectorAll("div > p")
+        for (let i = 0; i < ps.length; i++) {
+            ps[i].innerText="welcome to the jungle"
+        }
+    }
+
+//Exercise 5
+    
+//excercise 1
+const load=()=>{
     alert("WELCOME!!!")
+    document.querySelector("H1").addEventListener("mouseenter",addClassToTitle)
+    document.querySelector("H1").addEventListener("mouseleave",removeClassFromTitle)
 }
