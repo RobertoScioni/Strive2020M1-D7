c1ae1aaebd30e296910fc3081e3d8e9af3b683c9
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-10-06 16:19:59 +0200

    exercise up to 10 completed

--- M1-D7.js
+++ M1-D7.js
@@ -13,17 +13,68 @@
  
 //exercise 4
     const changeP=()=>{
-        ps=document.querySelectorAll("div > p")
+        let ps=document.querySelectorAll("div > p")
         for (let i = 0; i < ps.length; i++) {
             ps[i].innerText="welcome to the jungle"
         }
     }
 
 //Exercise 5
-    
+    const changeListTitle=()=>{
+        document.querySelector("#listTitle").innerText=document.querySelector("#newTitle").value
+    }
+//Exercise 6
+    const addToTheSecond=()=>{
+        let message=document.createTextNode(document.querySelector("#newElement").value)
+        let nuovo= document.createElement("li")
+        console.log(nuovo)
+        console.log(message)
+        nuovo.appendChild(message)
+        console.log(nuovo)
+        document.querySelector("#secondList").appendChild(nuovo)
+        
+    }
+//Exercise 7
+    const firstUlDisappear=()=>{
+        document.querySelector("ul").style.display="none"
+    }
+//exercise 8
+    const paintItGreen=()=>{
+        list=document.querySelectorAll("ul")
+        for (let i = 0; i < list.length; i++) {
+            list[i].style.background="green"
+        }
+    }
+//exercise 9
+    const zoomOn=(node)=>{
+        node.srcElement.style.fontSize="xxx-large"
+    }
+    const zoomOff=(node)=>{
+        node.srcElement.style.fontSize=""
+    }
+    const makeThemMagnifiable=()=>{
+        cells=document.querySelectorAll("td,bestPhone")
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].id="cell"+i
+            
+            cells[i].addEventListener("mouseenter",zoomOn)
+            cells[i].addEventListener("mouseleave",zoomOff)
+        }
+    }
+
+//excercise 10
+ const toggleShowImages=()=>{
+     pics=document.querySelectorAll("img")
+     let visibility=pics[0].style.display==="none"?"":"none"
+     console.log(visibility)
+     for (let i = 0; i < pics.length; i++) {
+        pics[i].style.display=visibility       
+     }
+ }
 //excercise 1
 const load=()=>{
-    alert("WELCOME!!!")
+   // alert("WELCOME!!!")
+    makeThemMagnifiable()
     document.querySelector("H1").addEventListener("mouseenter",addClassToTitle)
     document.querySelector("H1").addEventListener("mouseleave",removeClassFromTitle)
 }
